bf4be9871a95eba58130e01749e35420dfbb4b03
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-09-24 10:43:16 +0300

    added first file

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Введите n = ";
+    cin >> n;
+    cout << "Ваше число: " << n;
+    return 0;
+}
